832f1e2719b99ea3868448ee8cb023e325e111e3
yamamotomako <ymako@hgc.jp>
2017-04-26 13:17:49 +0900

    test

---
+++ setup.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+
+setup(name="depth_tool",
+	description="Calculating mean depth tool.",
+	url="https://github.com/yamamotomako/depth_tool",
+	package_dir=":scripts",
+	packages=["depth_tool"],
+	scripts=["depth_tool"]
+	)
+
+
