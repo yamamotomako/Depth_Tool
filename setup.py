8645806e5c61b6a031ecf54d97b5e86649612869
yamamotomako <ymako@hgc.jp>
2017-04-28 10:48:54 +0900

    modify

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 #! /usr/bin/env python
 
+from setuptools import setup
+
 setup(name="depth_tool",
-	description="Calculating mean depth tool.",
-	url="https://github.com/yamamotomako/calc_mean_depth",
-	package_dir=":scripts",
+	#description="Calculating mean depth tool.",
+	#url="https://github.com/yamamotomako/depth_tool",
+	package_dir={'':'scripts'},
 	packages=["depth_tool"],
 	scripts=["depth_tool"]
 	)
