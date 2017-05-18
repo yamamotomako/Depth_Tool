9734519c066e5247e74e1b12c9cda7c534ba6832
yamamotomako <ymako@hgc.jp>
2017-05-18 15:08:05 +0900

    renew

---
+++ run.py
@@ -0,0 +1,75 @@
+#! /usr/bin/env python
+
+import sys
+import os
+import re
+import pybedtools
+from collections import OrderedDict
+
+
+
+def calc_mean_depth(bamfile, baitfile, outfile):
+
+    print "Starting bedtools intersect..."
+
+    bam_p = pybedtools.BedTool(bamfile)
+    bed_p = pybedtools.BedTool(baitfile)
+    bam2ref = bam_p.intersect(bed_p, bed=True, wo=True).saveas(outfile+".ints")
+
+    print "Finished bedtools intersect..."
+
+
+    ref_dict = OrderedDict()
+
+    with open(outfile+".ints", "r") as f:
+        for ff in f:
+            line = ff.split("\t")
+            chrm = line[0]
+            match_start = line[1]
+            match_end = line[2]
+            ref_start = line[13]
+            ref_end = line[14]
+            match_base = int(line[len(line)-1])
+            other_info = "\t".join(line[15:])
+            key = ref_start + "-" + ref_end
+
+            if not ref_dict.has_key(key):
+                depth = match_base
+                ref_dict[key] = [chrm, match_start, match_end, ref_start, ref_end, depth, other_info]
+            else:
+                depth = ref_dict[key][5] + match_base
+                ref_dict[key][5] = depth
+
+
+
+    print "Making output file..."
+
+    #header
+    result_str = "\t".join(["chromesome:start-end","mean-depth"]) + "\n"
+
+    for k, v in ref_dict.items():
+        mean_depth = int(v[5]) / (int(v[4]) - int(v[3]))
+        result_str += "chr"+v[0]+":"+v[3]+"-"+v[4] + "\t" + str(mean_depth) + "\n"
+
+
+    with open(outfile, "w") as g:
+        g.write(result_str.strip())
+
+
+
+    print "Finished all process!"
+
+
+bamfile = sys.argv[1]
+baitfile = sys.argv[2]
+outfile = sys.argv[3]
+
+
+calc_mean_depth(bamfile, baitfile, outfile)
+
+
+sys.exit()
+
+
+
+
